d9d48821beb8f55dce6f0acd08b536e106ef805c
elj97 <1020194@student.sae.edu.au>
2020-07-01 11:15:54 +0800

    push1

---
+++ BigRobotsGoBoom/Assets/Scripts/FightingCombos.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum AttackType {  heavy = 0, light = 1, kick = 2 };
+public class FightingCombos : MonoBehaviour
+{
+    // these are what the player will press to do these attacks
+    [Header("Inputs")]
+    public KeyCode heavyKey;
+    public KeyCode lightKey;
+    public KeyCode kickKey;
+
+    // set up attacks
+    [Header("Attacks")]
+    public Attack heavyAttack;
+    public Attack lightAttack;
+    public Attack kickAttack;
+    public List<Combo> combos;
+    public float comboLeeway = 0.2f;
+
+    [Header("Components")]
+    public Animator ani;
+
+    Attack curAttack = null;
+    ComboInput lastInput = null;
+    List<int> currentCombos = new List<int>();
+
+    float timer = 0;
+    float leeway = 0;
+    bool skip = false;
+    void Start()
+    {
+        PrimeCombos();
+    }
+
+    void PrimeCombos()
+    {
+        for(int i = 0; i < combos.Count; i++)
+        {
+            Combo c = combos[i];
+            c.onInputted.AddListener(() =>
+            {
+                // Call attack function with the combo's attack
+                skip = true;
+                Attack(c.comboAttack);
+                ResetCombos();
+            });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curAttack != null)
+        {
+            if (timer > 0)
+                timer -= Time.deltaTime;
+            else
+                curAttack = null;
+
+            return;
+        }
+
+        if (currentCombos.Count > 0)
+        {
+            leeway += Time.deltaTime;
+            if (leeway >= comboLeeway)
+            {
+                if (lastInput != null)
+                {
+                    Attack(getAttackFromType(lastInput.type));
+                    lastInput = null;
+                }
+
+                ResetCombos();
+            }
+        }
+        else
+            leeway = 0;
+
+        ComboInput input = null;
+        if (Input.GetKeyDown(heavyKey))
+            input = new ComboInput(AttackType.heavy);
+        if (Input.GetKeyDown(lightKey))
+            input = new ComboInput(AttackType.light);
+        if (Input.GetKeyDown(kickKey))
+            input = new ComboInput(AttackType.kick);
+
+        if (input == null) return;
+        lastInput = input;
+
+        List<int> remove = new List<int>();
+        for (int i = 0; i < currentCombos.Count; i++)
+        {
+            Combo c = combos[currentCombos[i]];
+            if (c.continueCombo(input))
+                leeway = 0;
+            else
+                remove.Add(i);
+        }
+
+        if(skip)
+        {
+            skip = false;
+            return;
+        }
+
+        for (int i = 0; i < combos.Count; i++)
+        {
+            if (currentCombos.Contains(i)) continue;
+            if (combos[i].continueCombo(input))
+            {
+                currentCombos.Add(i);
+                leeway = 0;
+            }   
+        }
+
+        foreach (int i in remove)
+            currentCombos.RemoveAt(i);
+
+        if (currentCombos.Count <= 0)
+            Attack(getAttackFromType(input.type));
+    }
+
+    void ResetCombos()
+    {
+        leeway = 0;
+        for (int i = 0; i < currentCombos.Count; i++)
+        {
+            Combo c = combos[currentCombos[i]];
+            c.ResetCombo();
+        }
+
+        currentCombos.Clear();
+    }
+
+    void Attack(Attack att)
+    {
+        curAttack = att;
+        timer = att.length;
+        ani.Play(att.name, -1, 0);
+    }
+
+    Attack getAttackFromType(AttackType t)
+    {
+        if (t == AttackType.heavy)
+            return heavyAttack;
+        if (t == AttackType.light)
+            return lightAttack;
+        if (t == AttackType.kick)
+            return kickAttack;
+        return null;
+    }
+
+}
+// create attacks
+[System.Serializable]
+public class Attack
+{
+    public string name;
+    public float length;
+}
+
+[System.Serializable]
+public class ComboInput
+{
+    // hold info of what to press to initiate that combo attack
+    public AttackType type;
+    // Movement input for my precise combos
+
+    public ComboInput(AttackType t)
+    {
+        type = t;
+    }
+
+    public bool isSameAs(ComboInput test)
+    {
+        return (type == test.type); //Add && movement == test.movement
+    }
+}
+
+[System.Serializable]
+public class Combo
+{
+    public string name;
+    public List<ComboInput> inputs;
+    public Attack comboAttack;
+    public UnityEvent onInputted;
+    int curInput = 0;
+
+    public bool continueCombo(ComboInput i)
+    {
+        if(inputs[curInput].isSameAs(i)) 
+        {
+            curInput++;
+
+            if(curInput >= inputs.Count) // Finished the inputs and we should do the attack
+            {
+                onInputted.Invoke();
+                curInput = 0;
+            }
+            return true;
+        }
+        else
+        {
+            curInput = 0;
+            return false;
+        }
+    }
+
+    public ComboInput currentComboInput()
+    {
+        if (curInput >= inputs.Count) return null;
+        return inputs[curInput];
+    }
+
+    public void ResetCombo()
+    {
+        curInput = 0;
+    }
+}
