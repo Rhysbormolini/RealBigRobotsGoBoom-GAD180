6a3925b4df653d76bd60ce938ad09c5ad8741143
Rhysbormolini <61007759+Rhysbormolini@users.noreply.github.com>
2020-07-23 23:25:54 +0800

    i hate myself

--- BigRobotsGoBoom/Assets/Scripts/Player 1/PlayerOneMovement.cs
+++ BigRobotsGoBoom/Assets/Scripts/Player 1/PlayerOneMovement.cs
@@ -7,12 +7,7 @@ public class PlayerOneMovement : MonoBehaviour
 
     public float moveSpeed;
     public float jumpHeight;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+    
 
     // Update is called once per frame
     void FixedUpdate()
@@ -27,9 +22,10 @@ public class PlayerOneMovement : MonoBehaviour
             this.transform.position += this.transform.forward * Time.deltaTime * this.moveSpeed;
         }
 
-        if(Input.GetKey(KeyCode.W) == true && Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 0.01f)
+        if (Input.GetKey(KeyCode.W) == true && Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 0.01f)
         {
             this.GetComponent<Rigidbody>().velocity += Vector3.up * this.jumpHeight;
         }
     }
+
 }
--- BigRobotsGoBoom/Assets/Scripts/Player 2/PlayerTwoMovement.cs
+++ BigRobotsGoBoom/Assets/Scripts/Player 2/PlayerTwoMovement.cs
@@ -8,11 +8,6 @@ public class PlayerTwoMovement : MonoBehaviour
     public float moveSpeed;
     public float jumpHeight;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void FixedUpdate()
