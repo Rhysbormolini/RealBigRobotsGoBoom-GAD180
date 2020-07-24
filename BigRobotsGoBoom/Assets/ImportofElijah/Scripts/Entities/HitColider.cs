fd52ab4dfc7dc2d9fb2aff03320bbb66557c1fa6
elj97 <1020194@student.sae.edu.au>
2020-07-24 18:09:52 +0800

    poosh

--- BigRobotsGoBoom/Assets/ImportofElijah/Scripts/Entities/HitColider.cs
+++ BigRobotsGoBoom/Assets/ImportofElijah/Scripts/Entities/HitColider.cs
@@ -9,10 +9,10 @@ public class HitColider : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
 		Fighter somebody = other.gameObject.GetComponent<Fighter> ();
-        //if (owner.attacking) 
+        if (owner.attacking) 
         
 			if (somebody != null && somebody != owner) {
-				//somebody.hurt (damage);
+				somebody.hurt (damage);
                 Debug.Log("I hit " + somebody + " with " + attackName);
             }
 	
