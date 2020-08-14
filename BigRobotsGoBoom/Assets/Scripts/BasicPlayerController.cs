1b3b0f08a4cd4a46446f83f8fc085755b62c842e
Michael16219 <61007828+Michael16219@users.noreply.github.com>
2020-08-14 11:45:05 +0800

    sHoot in progress

--- BigRobotsGoBoom/Assets/Scripts/BasicPlayerController.cs
+++ BigRobotsGoBoom/Assets/Scripts/BasicPlayerController.cs
@@ -7,11 +7,13 @@ public class BasicPlayerController : MonoBehaviour
     public float moveSpeed = 1f;
 
     public float currentMoveSpeed;
+
+    protected Animator animator;
     void Update()
-    { 
+    {
         if (Input.GetKey(KeyCode.D) == true)
         {
-            this.transform.position += this.transform.forward* Time.deltaTime * this.moveSpeed;
+            this.transform.position += this.transform.forward * Time.deltaTime * this.moveSpeed;
             animator.SetBool("Walk Forward", true);
         }
         else
@@ -21,27 +23,12 @@ public class BasicPlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A) == true)
         {
-            this.transform.position -= this.transform.forward* Time.deltaTime* this.moveSpeed;
+            this.transform.position -= this.transform.forward * Time.deltaTime * this.moveSpeed;
             animator.SetBool("Walk Backward", true);
         }
         else
         {
             animator.SetBool("Walk Backward", false);
         }
-
-        //jump
-        if (Input.GetKey(KeyCode.W) == true && Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 0.01f)
-        {
-            this.GetComponent<Rigidbody>().velocity += Vector3.up * this.jumpHeight;
-        }
-
-        if (Input.GetKey(KeyCode.V) == true)
-        {
-             animator.SetBool("Hit_Sword_0", true);
-             Debug.Log("hit sword");
-        }
-        else
-        {
-             animator.SetBool("Hit_Sword_0", false);
-        }
     }
+}
