e3ac203b4c4e230188666ed47717485ce7fe89f8
Michael16219 <61007828+Michael16219@users.noreply.github.com>
2020-08-14 11:24:28 +0800

    shoot in progress

---
+++ BigRobotsGoBoom/Assets/Scripts/BasicPlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    public float currentMoveSpeed;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) == true) { this.transform.position += this.transform.forward * Time.deltaTime * this.moveSpeed; }
+        if (Input.GetKey(KeyCode.A) == true) { this.transform.position -= this.transform.forward * Time.deltaTime * this.moveSpeed; }
+    }
+}
